721539cc83147aec92ac861ac11e55dc9d4ac3a0
080Rishabh <rishabgujetiya28@gmail.com>
2025-02-21 13:21:21 +0530

    adding files

--- app.py
+++ app.py
@@ -87,6 +87,7 @@ db.create_all()
 from routes import *
 from category import *
 
-# if __name__=="__main__":
-#     app.run(debug=True)
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
